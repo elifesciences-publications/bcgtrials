a727ee8f18a935e811afa2ebdc2777567977774d
andrew <andrew@serverharpo.vet.cam.ac.uk>
2017-08-31 09:03:07 +0100

    Sims Run, Seg Fault issue fixed in bTB Model Code

--- include/PATHS.h
+++ include/PATHS.h
@@ -1,1 +1,1 @@
-#define PATH_TO_DATA string("/Users/andrewconlan/Documents/Papers/BCGTrials/Revision/Supplemental/data/")
+#define PATH_TO_DATA string("/home/andrew/Simulations/BCG/bcgtrials/data/")
